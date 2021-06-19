fefa1edf757d0375d52b1f0d9bc3282c3d9357a0
Taglinetrader <contact_ho@taglinetraders.com>
2021-06-19 22:58:36 +0530

    REMOVED UNWANTED CODE

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,21 +1,9 @@
 import express from "express";
 const router = express.Router();
-import {
-  login,
-  create,
-  getProfile,
-  updateProfile,
-  getAll,
-  remove,
-  getById,
-  update,
-} from "../controllers/userController.js";
+import { login, create } from "../controllers/userController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 router.route("/").post(create);
 router.route("/login").post(login);
-router.route("/").get(getAll);
-router.route("/profile").get(getProfile).put(updateProfile);
-router.route("/:id").delete(remove).get(getById).put(update);
 
 export default router;
